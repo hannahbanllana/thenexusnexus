a24dca4a7ec75ac26ed84cf7cc5490321a288807
hannahbanllana <hannahkwak2027@u.northwestern.edu>
2026-07-25 14:07:09 -0500

    update summer hours, add link to hours page on error because railway endpoint died

--- src/components/error-handler.jsx
+++ src/components/error-handler.jsx
@@ -1,4 +1,5 @@
 import "./error-handler.css";
+import { Link } from "react-router-dom";
 
 export default function ErrorHandler() {
   return (
@@ -6,6 +7,7 @@ export default function ErrorHandler() {
       <div className="error-content">
         <h2>error. gulp...</h2>
         <img src="/error.gif" alt="error." style={{ width: "250px" }} />
+        <Link to="/hours" className="hours-link">view hours</Link>
       </div>
     </div>
   );
--- src/pages/Hours.jsx
+++ src/pages/Hours.jsx
@@ -10,13 +10,23 @@ const HOURS = [
   { day: "saturday", time: "12:30 PM – 10:30 PM" },
 ];
 
+const SHOURS = [
+  { day: "sunday", time: "closed" },
+  { day: "monday", time: "11:00 AM - 2:00 PM" },
+  { day: "tuesday", time: "closed" },
+  { day: "wednesday", time: "2:00 PM - 5:00 PM" },
+  { day: "thursday", time: "11:00 AM - 2:00 PM" },
+  { day: "friday", time: "2:00 PM - 5:00 PM" },
+  { day: "saturday", time: "closed" },
+];
+
 export default function Hours() {
   return (
     <div className="hours">
       <div className="hours-content">
-        <h2>hours</h2>
+        <h2> summer hours</h2>
         <div className="hours-table">
-          {HOURS.map(({ day, time }) => (
+          {SHOURS.map(({ day, time }) => (
             <div className="hours-row" key={day}>
               <span className="hours-day">{day}</span>
               <span className="hours-time">{time}</span>
